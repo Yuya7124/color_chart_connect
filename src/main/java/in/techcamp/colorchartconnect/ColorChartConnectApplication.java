cda9d3af54cda19a1aa8f771612db65c9b417379
Yuya7124 <86750113+Yuya7124@users.noreply.github.com>
2023-09-25 22:55:45 +0900

    ファイル移動

---
+++ src/main/java/in/techcamp/colorchartconnect/ColorChartConnectApplication.java
@@ -0,0 +1,13 @@
+package in.techcamp.colorchartconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColorChartConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColorChartConnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/techcamp/colorchartconnect/ColorChartConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package in.techcamp.colorchartconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColorChartConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
