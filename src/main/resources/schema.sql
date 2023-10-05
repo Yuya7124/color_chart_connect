7654f4c1be158fe70d7434c0959c4c389ee4ca00
Yuya7124 <86750113+Yuya7124@users.noreply.github.com>
2023-10-05 23:57:33 +0900

    SQL設計中

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,18 +1,23 @@
 CREATE TABLE IF NOT EXISTS user {
-    nickname VARCHAR(256) NOT NULL,
+    nickname VARCHAR(256) NOT NULL PRIMARY KEY,
     email VARCHAR(256) NOT NULL,
     password VARCHAR(100) NOT NULL
 }
 
-CREATE TABLE IF NOT EXISTS color_chart (
-    product_id BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    part VARCHAR(256) NOT NULL,
-    maker VARCHAR(256) NOT NULL,
-    red INTEGER NOT NULL,
-    green INTEGER NOT NULL,
-    blue INTEGER NOT NULL,
-    coat INTEGER NOT NULL,
-    ratio INTEGER NOT NULL
-);
+--CREATE TABLE IF NOT EXISTS product (
+--    user_id VARCHAR(256) NOT NULL,
+--    color_chart
+--);
+--
+--CREATE TABLE IF NOT EXISTS color_chart (
+--    product_id BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+--    part VARCHAR(256) NOT NULL,
+--    maker VARCHAR(256) NOT NULL,
+--    red INT NOT NULL,
+--    green INT NOT NULL,
+--    blue INT NOT NULL,
+--    coat INT NOT NULL,
+--    ratio INT NOT NULL
+--);
 
 
