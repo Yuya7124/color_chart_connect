6fd36d2baae61b2fa192146abc08b8d3bc5c4c5b
Yuya7124 <86750113+Yuya7124@users.noreply.github.com>
2023-10-12 00:14:57 +0900

    バリエーション実装（途中）

--- src/main/java/in/techcamp/colorchartconnect/controller/SignupController.java
+++ src/main/java/in/techcamp/colorchartconnect/controller/SignupController.java
@@ -4,6 +4,7 @@ import in.techcamp.colorchartconnect.entity.SignupEntity;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,10 +22,9 @@ public class SignupController {
 
   //ユーザー登録
   @PostMapping("/signup")
-  public String postSignup(@ModelAttribute SignupEntity entity){
+  public String postSignup(@ModelAttribute @Validated SignupEntity entity) {
     log.info(entity.toString());
     //ログイン画面へ
     return "redirect:/login";
   }
-
 }
--- src/main/java/in/techcamp/colorchartconnect/entity/SignupEntity.java
+++ src/main/java/in/techcamp/colorchartconnect/entity/SignupEntity.java
@@ -1,12 +1,16 @@
 package in.techcamp.colorchartconnect.entity;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @AllArgsConstructor
 @Data
 public class SignupEntity {
+  @NotBlank
   private String nickname;
+  @NotBlank
   private String email;
+  @NotBlank
   private String password;
 }
