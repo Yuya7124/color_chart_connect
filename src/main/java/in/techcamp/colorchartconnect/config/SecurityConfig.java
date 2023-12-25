1ef833b7c7f4b0919fd2648e9507f0cf10fef12e
Yuya7124 <86750113+Yuya7124@users.noreply.github.com>
2023-12-25 23:17:57 +0900

    ログイン機能①

---
+++ src/main/java/in/techcamp/colorchartconnect/config/SecurityConfig.java
@@ -0,0 +1,45 @@
+package in.techcamp.colorchartconnect.config;
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    //ログイン時
+    http.formLogin(login -> login
+            .loginProcessingUrl("/login")
+            .loginPage("/login")
+            .defaultSuccessUrl("/")
+            .failureUrl("/login?error")
+            .permitAll()
+            //ログアウト時
+    ).logout(logout -> logout
+            .logoutSuccessUrl("/")
+            //ログイン不要部分
+    ).authorizeHttpRequests(authz -> authz
+            .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+            .requestMatchers("/").permitAll()
+            .requestMatchers("/general").hasRole("GENERAL")
+            .requestMatchers("/admin").hasRole("ADMIN")
+            .anyRequest().authenticated()
+    );
+    return http.build();
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+}
