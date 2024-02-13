18db18a2574955b60d224ecfe90ca709919985b1
Yuya7124 <86750113+Yuya7124@users.noreply.github.com>
2024-02-13 23:31:40 +0900

    ユーザ表示②

--- src/main/java/in/techcamp/colorchartconnect/controller/LoginController.java
+++ src/main/java/in/techcamp/colorchartconnect/controller/LoginController.java
@@ -1,18 +1,34 @@
 package in.techcamp.colorchartconnect.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import in.techcamp.colorchartconnect.form.SignupForm;
+import in.techcamp.colorchartconnect.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+
 @Controller
+@Slf4j
 public class LoginController {
+
+  @Autowired
+  private UserRepository userRepository;
+
   @GetMapping("/login")
   public String getLogin() {
     return "login/login";
   }
 
   @PostMapping("/login")
-  public String postLogin(){
+  public String postLogin(@PathVariable String userId, Model model, SignupForm form){
+    
+    model.addAttribute(userId, "userId");
+    userRepository.findLoginUser(userId);
+    log.info(form.toString());
     return "redirect:/";
   }
 }
