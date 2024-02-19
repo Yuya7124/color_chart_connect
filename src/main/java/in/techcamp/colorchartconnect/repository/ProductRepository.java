d64eb13250d68ae596855f96105169729d908592
Yuya7124 <86750113+Yuya7124@users.noreply.github.com>
2024-02-19 22:36:11 +0900

    ユーザ表示④

--- src/main/java/in/techcamp/colorchartconnect/repository/ProductRepository.java
+++ src/main/java/in/techcamp/colorchartconnect/repository/ProductRepository.java
@@ -11,11 +11,11 @@ public interface ProductRepository {
   @Select("select * from product")
   List<ProductEntity> findAll();
   @Insert("INSERT INTO product (product_name, color_chart, image_data, image_filename, comment) VALUES (#{product_name}, #{color_chart}, #{image_data}, #{image_filename}, #{comment})")
-  void insert(String product_name, String color_chart, byte[] image_data, String image_filename, String comment);
+  void insert(@Param("product_name") String product_name, @Param("color_chart")String color_chart, @Param("image_data")byte[] image_data, @Param("image_filename")String image_filename, @Param("comment")String comment);
   @Select("select * from product where product_id = #{product_id}")
   ProductEntity findById(long product_id);
   @Update("UPDATE product SET product_name = #{product_name}, color_chart = #{color_chart}, image_data = #{image_data}, image_filename = #{image_filename}, comment = #{comment} WHERE product_id = #{product_id}")
-  void update(long product_id, String product_name, String color_chart, byte[] image_data, String image_filename, String comment);
+  void update(@Param("product_id")long product_id, @Param("product_name")String product_name, @Param("color_chart")String color_chart, @Param("image_data")byte[] image_data, @Param("image_filename")String image_filename, @Param("comment")String comment);
   @Delete("delete from product WHERE product_id = #{product_id}")
   void deleteById(long product_id);
 }
